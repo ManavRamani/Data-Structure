e4cf07f09d65042d06945438c4a3c1b3121d6e03
Mr_Tech(Manav_Ramani) <104958217+ManavRamani@users.noreply.github.com>
2024-01-10 19:19:28 +0530

    Update arr_big_pointer.c
    change code pointer use

--- C Lang Lab/4 lab/arr_big_pointer.c
+++ C Lang Lab/4 lab/arr_big_pointer.c
@@ -2,26 +2,50 @@
 
 #include<stdio.h>
 void main(){
-	int size,*max,i;
-	
-    printf("Enter Size of Array : ");
-    scanf("%d",&size);
-    
-	int arr[size];
-	
-	for(i=0;i<size;i++){
-        printf("Enter Array[%d] : ",i);
-        scanf("%d",&arr[i]);
+
+    int n,i;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    
-    max=&arr[0];
-    
-	for(i=1;i<size;i++){
-       if(arr[i]>*max){
-       		max=&arr[i];
-	   }
+
+    int *ptr = arr;
+
+    int max = *ptr;
+    for (i = 1; i < n; i++) {
+        if (*(ptr + i) > max) {
+            max = *(ptr + i);
+        }
     }
+    printf("The largest element in the array is: %d\n", max);
+}	
+
+//  Extra Code ... not for pointer....................................
+//	int size,*max,i;
+	
+  //  printf("Enter Size of Array : ");
+    //scanf("%d",&size);
     
-    printf("\nmax is : %d",*max);
+	//int arr[size];
+	
+//	for(i=0;i<size;i++){
+ //       printf("Enter Array[%d] : ",i);
+   //     scanf("%d",&arr[i]);
+   // }
     
-}
+  //  max=&arr[0];
+    
+//	for(i=1;i<size;i++){
+  //     if(arr[i]>*max){
+       	//	max=&arr[i];
+	//   }
+    //}
+    
+//    printf("\nmax is : %d",*max);
+    
+//}
